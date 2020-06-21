9d5953eb1025b48f61147f6ee4f8341b1781e357
Haisheng Wu <haisheng.wu@okta.com>
2020-06-20 22:54:50 -0700

    more exercises.

--- exercises/traits/traits1.rs
+++ exercises/traits/traits1.rs
@@ -1,21 +1,25 @@
 // traits1.rs
 // Time to implement some traits!
-// 
+//
 // Your task is to implement the trait
 // `AppendBar' for the type `String'.
-// 
+//
 // The trait AppendBar has only one function,
 // which appends "Bar" to any object
 // implementing this trait.
 
-// I AM NOT DONE
 trait AppendBar {
     fn append_bar(self) -> Self;
 }
 
 impl AppendBar for String {
-    //Add your code here
+    fn append_bar(self) -> Self {
+        let mut re: String = String::new();
+        re.push_str(&self[..]);
+        re.push_str("Bar");
 
+        re
+    }
 }
 
 fn main() {
